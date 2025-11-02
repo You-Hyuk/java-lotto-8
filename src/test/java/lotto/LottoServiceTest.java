83b0f76a7c8150f25c7a286cc5c2e511fb03b24e
You-Hyuk <cxz6411@naver.com>
2025-11-03 04:51:27 +0900

    chore(Lotto): Lotto 클래스 domain 패키지 이동

--- src/test/java/lotto/LottoServiceTest.java
+++ src/test/java/lotto/LottoServiceTest.java
@@ -3,6 +3,7 @@ package lotto;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
+import lotto.domain.Lotto;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
 
