d1b4633085539de155beed8d33c0c092a18f9cdc
You-Hyuk <cxz6411@naver.com>
2025-11-03 15:21:06 +0900

    test(LottoGeneratorTest): LottoGeneratorTest 테스트 코드 수정
    LottosTest -> LottoGeneratorTest 이름 변경

--- src/test/java/lotto/LottoGeneratorTest.java
+++ src/test/java/lotto/LottoGeneratorTest.java
@@ -1,11 +1,15 @@
 package lotto;
 
+import lotto.domain.Lottos;
+import lotto.service.LottoGenerator;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
 
-public class LottosTest {
-    
+public class LottoGeneratorTest {
+
+    private final LottoGenerator lottoGenerator = new LottoGenerator();
+
     @Test
     @DisplayName("구입 금액 입력에 따른 개수 만큼 로또가 발행된다.")
     public void 구입_금액_입력에_따른_개수_만큼_로또가_발행된다() throws Exception {
@@ -13,9 +17,10 @@ public class LottosTest {
         int amount = 10000;
         
         //when
-        Lottos lottos = new Lottos(10000);
-        
+        int count = lottoGenerator.calculateLottoCount(amount);
+        Lottos lottos = lottoGenerator.issueLottos(count);
+
         //then
-        Assertions.assertThat(lottos.size()).isEqualTo(10);
+        Assertions.assertThat(lottos.copyOf().size()).isEqualTo(10);
     }
 }
