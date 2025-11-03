f2a99e7732ce07224b9983a26196b43188023d1d
You-Hyuk <cxz6411@naver.com>
2025-11-03 14:53:10 +0900

    feat(LottoGameService): 로또 총 수익률 계산

--- src/main/java/lotto/service/LottoGameService.java
+++ src/main/java/lotto/service/LottoGameService.java
@@ -1,5 +1,6 @@
 package lotto.service;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import lotto.domain.LottoGame;
@@ -11,5 +12,18 @@ public class LottoGameService {
     public Map<LottoRank, Integer> generateLottoGame(Lottos lottos, List<Integer> winningNumbers, int bonusNumber) {
         LottoGame lottoGame = new LottoGame(lottos, winningNumbers, bonusNumber);
         return lottoGame.calculateResults();
+    }
+
+    public double calculateProfitRate(long purchaseAmount, Map<LottoRank, Integer> lottoRankMap) {
+        long totalPrize = calculateTotalPrize(lottoRankMap);
+        double profitRate = (double) totalPrize / purchaseAmount * 100;
+        return Math.round(profitRate * 10) / 10.0;
+    }
+
+    public long calculateTotalPrize(Map<LottoRank, Integer> lottoRankMap) {
+        return Arrays.stream(LottoRank.values())
+                .filter(lottoRank -> lottoRank != LottoRank.NONE)
+                .mapToLong(lottoRank -> (long) lottoRank.getPrize() * lottoRankMap.getOrDefault(lottoRank, 0))
+                .sum();
     }
 }
