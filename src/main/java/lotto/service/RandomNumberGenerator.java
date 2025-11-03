7f5db25e4f365a52f85034ecefa9d60eeb84a149
You-Hyuk <cxz6411@naver.com>
2025-11-03 15:43:30 +0900

    fix(RandomNumberGenerator): 무작위 번호 생성 메소드 변경
    Randoms.pickNumberInRange() 메소드를 사용 후 중복 검증 X
    Randoms.pickUniqueNumbersInRange() 메소드 사용

--- src/main/java/lotto/service/RandomNumberGenerator.java
+++ src/main/java/lotto/service/RandomNumberGenerator.java
@@ -1,33 +1,12 @@
 package lotto.service;
 
 import camp.nextstep.edu.missionutils.Randoms;
-import java.util.ArrayList;
 import java.util.List;
 
 public class RandomNumberGenerator {
 
     public List<Integer> generateRandomNumbers(int size, int min, int max) {
-        List<Integer> randomNumbers;
-
-        do {
-            randomNumbers = new ArrayList<>();
-            for (int count = 0; count < size; count++) {
-                randomNumbers.add(generateRandomNumber(min, max));
-            }
-        } while (checkDuplicateNumber(randomNumbers));
-
-        return randomNumbers;
+        return Randoms.pickUniqueNumbersInRange(min, max, size);
     }
 
-    private int generateRandomNumber(int min, int max) {
-        return Randoms.pickNumberInRange(min, max);
-    }
-
-    private boolean checkDuplicateNumber(List<Integer> numbers) {
-        long distinctCount = numbers.stream()
-                .distinct()
-                .count();
-
-        return distinctCount != numbers.size();
-    }
 }
