87db5d8dd64a1915429ba2e78c7a5b586f24479c
You-Hyuk <cxz6411@naver.com>
2025-11-03 11:48:31 +0900

    feat(InputValidator): 당첨 번호 입력 중복, 개수, 범위 검증
    LottoConstant -> LottoConstants 클래스명 변경

--- src/main/java/lotto/enums/ErrorMessage.java
+++ src/main/java/lotto/enums/ErrorMessage.java
@@ -3,7 +3,10 @@ package lotto.enums;
 public enum ErrorMessage {
     INVALID_NUMBER_INPUT("입력값은 숫자여야 합니다."),
     INVALID_PURCHASE_AMOUNT("구입 금액은 1000원 단위로 나누어 떨어져야 합니다."),
-    WINNING_NUMBERS_ENDS_WITH_DELIMITER("당첨 번호 입력은 쉼표(,)로 끝날 수 없습니다.");
+    WINNING_NUMBERS_ENDS_WITH_DELIMITER("당첨 번호 입력은 쉼표(,)로 끝날 수 없습니다."),
+    LOTTO_NUMBER_OUT_OF_BOUND("로또 번호는 1 이상 45 이하의 숫자여야 합니다."),
+    INVALID_LOTTO_NUMBER_COUNT("로또 번호는 6개의 숫자로 이루어져야 합니다."),
+    DUPLICATE_NUMBER("로또 번호는 중복될 수 없습니다.");
 
     private final String message;
 
--- src/main/java/lotto/validation/InputValidator.java
+++ src/main/java/lotto/validation/InputValidator.java
@@ -1,13 +1,14 @@
 package lotto.validation;
 
+import java.util.List;
 import lotto.enums.Delimiter;
 import lotto.enums.ErrorMessage;
-import lotto.enums.LottoConstant;
+import lotto.enums.LottoConstants;
 
 public class InputValidator {
 
     public void validatePurchaseAmount(long purchaseAmount) {
-        int price = LottoConstant.LOTTO_PRICE.getValue();
+        int price = LottoConstants.LOTTO_PRICE.getValue();
 
         if (purchaseAmount < price || purchaseAmount % price != 0) {
             throw new IllegalArgumentException(ErrorMessage.INVALID_PURCHASE_AMOUNT.getMessage());
@@ -17,6 +18,34 @@ public class InputValidator {
     public void validateRawWinningNumbers(String rawWinningNumbers) {
         if (rawWinningNumbers.endsWith(Delimiter.WINNING_NUMBERS_DELIMITER.getDelimiter())) {
             throw new IllegalArgumentException(ErrorMessage.WINNING_NUMBERS_ENDS_WITH_DELIMITER.getMessage());
+        }
+    }
+
+    public void validateWinningNumbers(List<Integer> winningNumbers) {
+        validateNumberCount(winningNumbers);
+        validateDuplicateNumber(winningNumbers);
+        winningNumbers.forEach(this::validateNumberRange);
+    }
+
+    private void validateDuplicateNumber(List<Integer> winningNumbers) {
+        long distinctCount = winningNumbers.stream()
+                .distinct()
+                .count();
+
+        if (distinctCount != LottoConstants.LOTTO_NUMBER_COUNT.getValue()) {
+            throw new IllegalArgumentException(ErrorMessage.DUPLICATE_NUMBER.getMessage());
+        }
+    }
+
+    private void validateNumberCount(List<Integer> winningNumbers) {
+        if (winningNumbers.size() != LottoConstants.LOTTO_NUMBER_COUNT.getValue()) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_LOTTO_NUMBER_COUNT.getMessage());
+        }
+    }
+
+    private void validateNumberRange(int number) {
+        if (number < LottoConstants.MIN_NUMBER.getValue() || number > LottoConstants.MAX_NUMBER.getValue()) {
+            throw new IllegalArgumentException(ErrorMessage.LOTTO_NUMBER_OUT_OF_BOUND.getMessage());
         }
     }
 }
